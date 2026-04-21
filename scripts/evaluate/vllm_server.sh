600fe42b79ba9c4ee688ebca5decd5be2a715bc6
bingxing_sj <bingxing_sj@xx.com>
2026-04-21 23:39:41 +0800

    update vllm_server.sh

--- scripts/evaluate/vllm_server.sh
+++ scripts/evaluate/vllm_server.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 source ~/.bashrc
-source /share/home/sxjiang/miniconda3/bin/activate 
-conda activate verl_5_2
+source ~/miniconda3/bin/activate
+conda activate vel_5
 
-MODEL_PATH=/share/home/sxjiang/myproject/LHTIR/checkpoints/merged_checkpoints/qwen3-4b-ftrl_multiturn-no_kl_no_ent-n_16-MATCHTIR_KM-24
-CUDA_VISIBLE_DEVICES=4,5 vllm serve $MODEL_PATH \
-    --served-model-name MatchTIR \
-    --tensor-parallel-size 1 \
-    --data-parallel-size 2 \
+MODEL_PATH=/root/model/Qwen3-30B-A3B-Thinking-2507
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 vllm serve $MODEL_PATH \
+    --served-model-name Qwen3-30B-A3B-Thinking-2507 \
     --gpu-memory-utilization 0.9 \
+    --tensor-parallel-size 2 \
+    --reasoning-parser deepseek_r1 \
     --trust-remote-code \
     --uvicorn-log-level debug \
     --host 0.0.0.0 \
     --port 7899 
 
-curl http://localhost:7899/v1/models
+curl http://61.161.168.20:7899/v1/models
 
-curl http://localhost:7899/v1/chat/completions -H "Content-Type: application/json" -d '{"model": "MatchTIR", "messages": [{"role": "user", "content": "Hello, how are you?"}]}'
+curl http://localhost:7899/v1/chat/completions -H "Content-Type: application/json" -d '{"model": "Qwen3-30B-A3B-Thinking-2507", "messages": [{"role": "user", "content": "Hello, who are you?"}]}'
